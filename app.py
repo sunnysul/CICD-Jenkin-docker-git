22e47576f4ca4e929d359273500ed61fe76bb983
Sunny-PC <sunnysul57@gmail.com>
2026-05-01 07:52:08 +0530

    Update index route response to include server status message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return 'Hello, World! server is running on port 8000'
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
